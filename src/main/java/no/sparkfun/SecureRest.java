b48bc08f21c5af91a5eb53f73c51dc42af5802ba
Frode Bjerkenes <frode.bjerkenes@gmail.com>
2017-03-15 00:18:10 +0100

    initial

---
+++ src/main/java/no/sparkfun/SecureRest.java
@@ -0,0 +1,33 @@
+package no.sparkfun;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class SecureRest {
+
+    public static void main(String[] args) throws Exception {
+        //secure("/programmering/sparkfun/src/main/resources/key/keystore.jks", "_Salty29", null, null);
+        Path file = Paths.get("/tmp/userResponse.txt");
+
+
+        get("/hello", (req, res) -> "Hello World");
+
+
+        //post("/userResponse", "application/json", (req, res) -> "200 Ok");
+        post("/userResponse", (req, res) -> {
+            List<String> lines = new ArrayList<>();
+            lines.add(req.body());
+            Files.write(file, lines, UTF_8, APPEND, CREATE);
+            return "200 Ok";
+        });
+    }
+}
