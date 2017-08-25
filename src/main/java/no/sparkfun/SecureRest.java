3fdf1b5d718ca82f1306a6c2ebe24eb0c2955dce
Frode Bjerkenes <frode.bjerkenes@telenor.com>
2017-08-25 09:21:13 +0200

    set port 8080 + /error endpoint

--- src/main/java/no/sparkfun/SecureRest.java
+++ src/main/java/no/sparkfun/SecureRest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 import static spark.Spark.post;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.StandardOpenOption.APPEND;
@@ -18,8 +19,13 @@ public class SecureRest {
         //secure("/programmering/sparkfun/src/main/resources/key/keystore.jks", "_Salty29", null, null);
         Path file = Paths.get("/tmp/userResponse.txt");
 
+        port(8080);
 
         get("/hello", (req, res) -> "Hello World");
+
+        get("/error", (req, res) -> {
+            throw new RuntimeException("Calling /error url");
+        });
 
 
         //post("/userResponse", "application/json", (req, res) -> "200 Ok");
