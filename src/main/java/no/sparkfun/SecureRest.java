1bc6c00bd35b75c2b44ae5bddd0bdce45241c04a
Frode Bjerkenes <frode.bjerkenes@telenor.com>
2017-09-19 15:02:10 +0200

    Added logging

--- src/main/java/no/sparkfun/SecureRest.java
+++ src/main/java/no/sparkfun/SecureRest.java
@@ -1,5 +1,8 @@
 package no.sparkfun;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,17 +18,19 @@ import static java.nio.file.StandardOpenOption.CREATE;
 
 public class SecureRest {
 
+    public static Logger log = LoggerFactory.getLogger(SecureRest.class);
+
     public static void main(String[] args) throws Exception {
+
+
         //secure("/programmering/sparkfun/src/main/resources/key/keystore.jks", "_Salty29", null, null);
         Path file = Paths.get("/tmp/userResponse.txt");
 
         port(8080);
 
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> hello());
 
-        get("/error", (req, res) -> {
-            throw new RuntimeException("Calling /error url");
-        });
+        get("/error", (req, res) -> error());
 
 
         //post("/userResponse", "application/json", (req, res) -> "200 Ok");
@@ -35,5 +40,15 @@ public class SecureRest {
             Files.write(file, lines, UTF_8, APPEND, CREATE);
             return "200 Ok";
         });
+    }
+
+    private static String hello(){
+        log.info("Calling hello");
+        return "Hello World";
+    }
+
+    private static String error(){
+        log.error("Calling error");
+        throw new RuntimeException("Calling /error url");
     }
 }
