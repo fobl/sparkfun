3be0cef3c86be85a6984b09061ea044d8c2df540
Frode Bjerkenes <frode.bjerkenes@telenor.com>
2018-03-21 22:52:21 +0100

    slow running process + rediswrite + redisread

--- src/main/java/no/sparkfun/SecureRest.java
+++ src/main/java/no/sparkfun/SecureRest.java
@@ -2,12 +2,14 @@ package no.sparkfun;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
@@ -20,8 +22,9 @@ public class SecureRest {
 
     public static Logger log = LoggerFactory.getLogger(SecureRest.class);
 
-    public static void main(String[] args) throws Exception {
+    public static String redishost = "127.0.0.1";
 
+    public static void main(String[] args) throws Exception {
 
         //secure("/programmering/sparkfun/src/main/resources/key/keystore.jks", "_Salty29", null, null);
         Path file = Paths.get("/tmp/userResponse.txt");
@@ -32,6 +35,12 @@ public class SecureRest {
 
         get("/error", (req, res) -> error());
 
+        get("/redisread", (req, res) -> redisread());
+
+        get("/rediswrite", (req, res) -> rediswrite());
+
+        get("/slow", (req, res) -> slow());
+
 
         //post("/userResponse", "application/json", (req, res) -> "200 Ok");
         post("/userResponse", (req, res) -> {
@@ -40,6 +49,28 @@ public class SecureRest {
             Files.write(file, lines, UTF_8, APPEND, CREATE);
             return "200 Ok";
         });
+    }
+
+    private static String slow() {
+        try {
+            Thread.sleep(600*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "finished";
+    }
+
+    private static String rediswrite() {
+        Jedis jedis = new Jedis(redishost);
+        Random r = new Random();
+        jedis.set("foo", "bar ");
+//        return "ok";
+        return jedis.get("foo");
+    }
+
+    private static Object redisread() {
+        Jedis jedis = new Jedis(redishost);
+        return jedis.get("bar");
     }
 
     private static String hello(){
